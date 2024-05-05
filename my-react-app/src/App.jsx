0f4f793c61fedc7b828a698d8e1599782309f8d1
tasniaiffat <tasniaiffat@gmail.com>
2024-05-05 10:24:01 +0600

    login signup front end

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SignupPage from './SignUpPage/SignupPage.jsx';
 import LoginPage from './LoginPage/LoginPage.jsx';
 import Login from './LoginTasnia/Login.jsx';
+import SignupBody from './SignupBody/SignupBody.jsx';
 
 function App() {
 
   return (
     <Router>
             <Routes>
-                <Route path="/" element={<Login/>} />
-                {/* <Route path="/" element={<SignupPage/>} />
-                <Route path="/login" element={<Login/>} /> */}
+            {/* <Route path="/" element={<Login/>} />
+                <Route path="/" element={<SignupBody/>} /> */}
+                <Route path="/" element={<SignupBody/>} />
+                <Route path="/login" element={<Login/>} />
             </Routes>
         </Router>
   );
--- my-react-app/src/SignupBody/SignupBody.jsx
+++ my-react-app/src/SignupBody/SignupBody.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useNavigate } from 'react-router-dom';
+import Header from '../Header/Header';
+import './SignupBody.css'
 
 function SignupBody(){
 
@@ -68,71 +70,74 @@ function SignupBody(){
       };
 
     return (
-        <div>
-            <h2>Sign Up</h2>
+        <>
+        <div className='wrapper'>
+            <h1>Sign Up</h1>
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>First Name: </label>
-                    <input type="text" id="FirstName" placeholder="Farhan" name = "first_name" value={formData.first_name} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>First Name: </label> */}
+                    <input type="text" id="FirstName" placeholder="First Name" name = "first_name" value={formData.first_name} onChange={handleChange} required />
+                    {/* <input type="text" id="LastName" placeholder="Last Name" name = "last_name" value={formData.last_name} onChange={handleChange} required /> */}
                 </div>
 
-                <div>
-                    <label>Last Name: </label>
-                    <input type="text" id="LastName" placeholder="Rahman" name = "last_name" value={formData.last_name} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Last Name: </label> */}
+                    <input type="text" id="LastName" placeholder="Last Name" name = "last_name" value={formData.last_name} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Email: </label>
-                    <input type="email" id="Email" placeholder="farhan@gmail.com" name = "email" value={formData.email} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Email: </label> */}
+                    <input type="email" id="Email" placeholder="Email Address" name = "email" value={formData.email} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Registration Number: </label>
-                    <input type="text" id="RegNo" placeholder="2020215666" name = "reg_number" value={formData.reg_number} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Registration Number: </label> */}
+                    <input type="text" id="RegNo" placeholder="Registration Number e.g. 2020415637" name = "reg_number" value={formData.reg_number} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Department: </label>
-                    <input type="text" id="Dept" placeholder="CSE" name = "department" value={formData.department} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Department: </label> */}
+                    <input type="text" id="Dept" placeholder="Department e.g. CSE" name = "department" value={formData.department} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Session: </label>
-                    <input type="text" id="Session" placeholder="2020-21" name = "session" value={formData.session} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Session: </label> */}
+                    <input type="text" id="Session" placeholder="Session e.g 2020-21" name = "session" value={formData.session} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Password: </label>
-                    <input type="password" id="Password" name = "password" value={formData.password} onChange={handleChange} required minLength="8" />
+                <div className="input-box">
+                    {/* <label>Password: </label> */}
+                    <input type="password" id="Password" placeholder= "Set Password" name = "password" value={formData.password} onChange={handleChange} required minLength="8" />
                 </div>
 
-                <div>
-                    <label>Confirm Password: </label>
-                    <input type="password" id="Confirm" name = "confirm_password" value={formData.confirm_password} onChange={handleChange} required minLength="8" />
+                <div className="input-box">
+                    {/* <label>Confirm Password: </label> */}
+                    <input type="password" id="Confirm" placeholder= "Confirm Password" name = "confirm_password" value={formData.confirm_password} onChange={handleChange} required minLength="8" />
                 </div>
 
-                <div>
-                    <label>Codeforces Handle: </label>
-                    <input type="text" id="CfHandle" name = "cf_handle" value={formData.cf_handle} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Codeforces Handle: </label> */}
+                    <input type="text" id="CfHandle" placeholder= "Codeforces Handle" name = "cf_handle" value={formData.cf_handle} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Codechef Handle: </label>
-                    <input type="text" id="CCHandle" name = "codechef_handle" value={formData.codechef_handle} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Codechef Handle: </label> */}
+                    <input type="text" id="CCHandle" placeholder= "Codechef Handle" name = "codechef_handle" value={formData.codechef_handle} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>AtCoder Handle: </label>
-                    <input type="text" id="AtcoderHandle" name = "atcoder_handle" value={formData.atcoder_handle} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>AtCoder Handle: </label> */}
+                    <input type="text" id="AtcoderHandle" placeholder= "Atcoder Handle" name = "atcoder_handle" value={formData.atcoder_handle} onChange={handleChange} required />
                 </div>
 
-                <div>
-                    <label>Vjudge Handle: </label>
-                    <input type="text" id="VjudgeHandle" name = "vjudge_handle" value={formData.vjudge_handle} onChange={handleChange} required />
+                <div className="input-box">
+                    {/* <label>Vjudge Handle: </label> */}
+                    <input type="text" id="VjudgeHandle" placeholder= "Vjudge Handle" name = "vjudge_handle" value={formData.vjudge_handle} onChange={handleChange} required />
                 </div>
-                <input type="submit" />
+                <button type='submit'>Sign Up</button>
             </form>
-        </div>  
+        </div> 
+        </> 
     );
 }
 
