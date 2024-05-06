bcdb8afbb2054aeccbb5deb9d0b79ba968261106
tasniaiffat <tasniaiffat@gmail.com>
2024-05-06 18:53:32 +0600

    Topbar created

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,39 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './LoginTasnia/Login.jsx';
-import SignupBody from './SignupBody/SignupBody.jsx';
+import { ColorModeContext, useMode } from "./theme.js";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./LoginTasnia/Login.jsx";
+import SignupBody from "./SignupBody/SignupBody.jsx";
+import Topbar from "./scenes/global/Topbar.jsx";
+
+// function App() {
+//   const [theme, colorMode] = useMode();
+//   return (
+//     <Router>
+//           <Routes>
+//             {/* <Route path="/" element={<Login/>} />
+//                 <Route path="/" element={<SignupBody/>} /> */}
+//             <Route path="/" element={<SignupBody />} />
+//             <Route path="/login" element={<Login />} />
+//           </Routes>
+//     </Router>
+//   );
+// }
 
 function App() {
-
+  const [theme, colorMode] = useMode();
   return (
-    <Router>
-            <Routes>
-            {/* <Route path="/" element={<Login/>} />
-                <Route path="/" element={<SignupBody/>} /> */}
-                <Route path="/" element={<SignupBody/>} />
-                <Route path="/login" element={<Login/>} />
-            </Routes>
-        </Router>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
-export default App
+export default App;
